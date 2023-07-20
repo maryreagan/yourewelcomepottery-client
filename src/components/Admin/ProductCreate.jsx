5fedde9d3feb5dd27fec623f9a3ee5bfd2a30e8f
Melissa Dufrechou <dufrechou82@gmail.com>
2023-07-19 22:19:46 -0500

    accepts image and form data and sends successfully to server. Need to update tag input to be dropdown

--- src/components/Admin/ProductCreate.jsx
+++ src/components/Admin/ProductCreate.jsx
@@ -1,53 +1,62 @@
-import React, { useId, useState }from 'react'
+import React, { useEffect, useId, useState }from 'react'
+import axios from 'axios'
 
 function ProductCreate() {
-const [ selectedFile, setSlectedFile ] = useState(null)
+const [ selectedFile, setSelectedFile ] = useState(null)
+const [ altText, setAltText ] = useState('')
+const [ productName , setProductName ] = useState('')
+const [ description, setDescription ] = useState('')
+const [price, setPrice ] = useState(0)
+const [ quantity, setQuantity ] = useState(0)
+const [ tag, setTag ] = useState("")
 
 const upload = async (e) => {
     e.preventDefault()
 
-    //Update state
-    setSlectedFile(e.target.files[0])
-    
     //Create an object of formData
 
     let formData = new FormData();
 
     //Add the file to formData
     formData.append('file', selectedFile)
+    formData.append('altText', altText)
+    formData.append('productName', productName)
+    formData.append('description', description)
+    formData.append('price', price)
+    formData.append('quantity', quantity)
+    formData.append('tag', tag)
 
-    console.log(selectedFile)
-
-    //Submit form-data
+    // Submit form-data
     try{
-
-        const respoonse = await axios.post('http://localhost:4000/product/create', formData, {
+            const response = await axios.post('http://127.0.0.1:4000/products/create', 
+            formData,{
             headers: {
                 'Content-Type':'multipart/form-data'
-            }
+            },
+           
             })
     }catch(err){
         console.log(err)
     }
         
-}
+    }
+
 
     return (
     <>
-        <h3>
-            aws s3 upload
-        </h3>
+       
         <form action="POST">
         <input
         type="file"
         accept="image/*"
-        onChange={upload}
+        onChange={e => setSelectedFile(e.target.files[0])}
         />
         <input
             type="altText"
             name="altText"
             id="altText"
             value={altText}
+            onChange={e => setAltText(e.target.value)}
             placeholder="altText for image"
         />
                 <input
@@ -55,6 +64,7 @@ const upload = async (e) => {
             name="productName"
             id="productName"
             value={productName}
+            onChange={e => setProductName(e.target.value)}
             placeholder="Enter Product Name"
         />
                 <input
@@ -63,6 +73,7 @@ const upload = async (e) => {
             id="price"
             accept='number'
             value={price}
+            onChange={e => setPrice(e.target.value)}
             placeholder="Enter price (numbers only)"
         />
                 <input
@@ -70,6 +81,7 @@ const upload = async (e) => {
             name="description"
             id="description"
             value={description}
+            onChange={e => setDescription(e.target.value)}
             placeholder="Product Description"
         />
                 <input
@@ -78,6 +90,7 @@ const upload = async (e) => {
             id="quantity"
             accept="number"
             value={quantity}
+            onChange={e => setQuantity(e.target.value)}
             placeholder="quantity"
         />
         {/* For this item, I want to create a drop down menu */}
@@ -86,8 +99,10 @@ const upload = async (e) => {
             name="tag"
             id="tag"
             value={tag}
+            onChange={e => setTag(e.target.value)}
             placeholder="tag"
         />
+        <button type="submit" onClick={upload}>Submit</button>
 
         </form>
     </>
