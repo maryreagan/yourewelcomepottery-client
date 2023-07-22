6f47f46ae9ec3364339126c64c636af34521a2e2
Melissa Dufrechou <114317218+dufmel@users.noreply.github.com>
2023-07-22 08:16:50 -0500

    Melissa routers (#5)
    * displaying and filtering items. Added bio page
    
    * delted a folder

--- src/AWS-config.js
+++
@@ -1,7 +0,0 @@
-require ("dotenv").config()
-import AWS from "aws-sdk";
-
-AWS.config.update({
-  accessKeyId: "YOUR_ACCESS_KEY",
-  secretAccessKey: "YOUR_SECRET_KEY",
-});
---
+++ src/components/Bio/Bio.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import headshot from "/headshot.jpeg"
+import "./Bio.css"
+
+function Bio() {
+  return (
+    <div className="bio-container">
+        <img src={headshot} alt="<Emma's headshot>" className="headshot"/>
+        <div className='biocard'>
+        <h1>Emma</h1>
+        <p>I'm a laweyer turned potter based in Nashville, TN. I love to make things.</p>
+
+        </div>
+    </ div>
+  )
+}
+
+export default Bio
