6c104cbc1a024501f15b4fe9069ff2c71a51cc63
Melissa Dufrechou <114317218+dufmel@users.noreply.github.com>
2023-07-24 13:31:33 -0500

    Jon3 (#7)
    * fixed admin
    
    * added footer
    
    ---------
    
    Co-authored-by: Jonathan Amsalem <jonathan.amsalem@gmail.com>

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./footer.css"
+function Footer() {
+  return (
+    <div>
+      <h2>GET IN TOUCH</h2>
+     <h3>Phone Number : 111-111-1111</h3>
+      <h3>Location: 309 Plus Park DriveStudio 147, Nashville, TN 37217</h3>
+      <h3>Hours of Operations: By Appointment Only</h3>
+        
+        
+        
+        </div>
+  )
+}
+
+export default Footer
