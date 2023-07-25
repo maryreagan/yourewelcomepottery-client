8db8c72185d5d83aa6fe2ad31f97fe8fb638d293
Jonathan Amsalem <jonathan.amsalem@gmail.com>
2023-07-25 13:49:59 -0400

    footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
+import MarkunreadIcon from '@mui/icons-material/Markunread';
 import "./footer.css"
 function Footer() {
+  let handleEmail = () => {
+    let email = "jonathan.amsalem@gmail.com"
+    let mailTo = `mailto:${email}`
+    window.location.href = mailTo
+  }
   return (
-    <div>
-      <h2>GET IN TOUCH</h2>
-     <h3>Phone Number : 111-111-1111</h3>
-      <h3>Location: 309 Plus Park DriveStudio 147, Nashville, TN 37217</h3>
-      <h3>Hours of Operations: By Appointment Only</h3>
+    <div id="footer">
+      <h3 >GET IN TOUCH</h3>
+      <h4>Location: 309 Plus Park DriveStudio 147, Nashville, TN 37217</h4>
+      <h4>Hours of Operations: By Appointment Only</h4>
+      <button onClick={handleEmail}>
+      <MarkunreadIcon fontSize="large" />
+      </button>
         
         
-        
+    
         </div>
   )
 }
