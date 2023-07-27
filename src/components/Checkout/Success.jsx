7d4cf2e00e6d44809c455683c9a290a0787a0c97
Jonathan Amsalem <jonathan.amsalem@gmail.com>
2023-07-27 17:48:11 -0400

    cart

--- src/components/Checkout/Success.jsx
+++ src/components/Checkout/Success.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Button } from "@mui/material";
 
+
+
 function Success() {
+  // let body = {
+  //   url : window.location.href
+  // }
+  // let requestOptions = {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(body),
+  // }
+  useEffect(() => {
+    fetch(`http://127.0.0.1:4000/products/retrieve/${window.location.href}`)
+      .then(console.log('success'))
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
   return (
     <div
       style={{
