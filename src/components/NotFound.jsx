c9f9a0ab26d65c4f9d9d980fcbb5aa0d4d162b0e
Melissa Dufrechou <dufrechou82@gmail.com>
2023-07-31 13:52:58 -0500

    tweaks

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import { Typography } from '@mui/material';
+
 import "./NotFound.css"
+
 function NotFound() {
   return (
     <>
-      <h2>Oops! There is nothing here</h2>
-      <a href="/">
-      <h3 id ="goHomeBtn">Click here to go home!</h3>
-      </a>
+      <Typography variant="h5" color="primary" align="center" marginTop="2em">
+        Looks like there's nothing on this page!
+      </Typography>
+
+      <Link to="/">
+        <Typography color="primary" align="center" marginTop="1em" sx={{ cursor: "pointer"}} >
+          Click to return to homepage
+        </Typography>
+      </Link>
+
     </>
-  )
+  );
 }
 
 export default NotFound
