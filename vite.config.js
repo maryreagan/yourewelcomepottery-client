7fc4c1ace12d5dffbf1ad14b24e9d86d8b2f25db
Melissa Dufrechou <114317218+dufmel@users.noreply.github.com>
2023-08-03 09:51:40 -0500

    Develop (#30)
    * file upload on front end
    
    * form for image and product details
    
    * accepts image and form data and sends successfully to server. Need to update tag input to be dropdown
    
    * built out landing page display (#2)
    
    * built out landing page display (#3)
    
    * Jon (#4)
    
    * Admin page
    
    * package
    
    * Melissa routers (#5)
    
    * displaying and filtering items. Added bio page
    
    * delted a folder
    
    * fully functioning admin (#6)
    
    Co-authored-by: Melissa Dufrechou <114317218+dufmel@users.noreply.github.com>
    
    * Jon3 (#7)
    
    * fixed admin
    
    * added footer
    
    ---------
    
    Co-authored-by: Jonathan Amsalem <jonathan.amsalem@gmail.com>
    
    * added nav bar (#9)
    
    * Melissa images (#10)
    
    * added updated images
    
    * basic functioning cart
    
    * Jon3 (#11)
    
    * fixed admin
    
    * added footer
    
    * footer
    
    ---------
    
    Co-authored-by: Jonathan Amsalem <jonathan.amsalem@gmail.com>
    
    * Melissa images (#12)
    
    * added updated images
    
    * basic functioning cart
    
    * updated shopping cart
    
    * Melissa styling (#13)
    
    * added updated images
    
    * basic functioning cart
    
    * updated shopping cart
    
    * Can only incremeent to DB value and decrement to 0 functions working properly
    
    * Jon4 (#14)
    
    * footer
    
    * footer done
    
    * instagram
    
    * fixed
    
    * fixed ratio
    
    * fixed style
    
    * Jon4 (#15)
    
    * footer
    
    * footer done
    
    * instagram
    
    * fixed
    
    * fixed ratio
    
    * fixed style
    
    * bio
    
    * contact
    
    * fixed bugs admin page
    
    * Melissa wednesday (#17)
    
    * shopping cart styling completed
    
    * updatd card color
    
    * Updated tags
    
    * small tweeks
    
    * 404 (#18)
    
    * added flag if not products in db
    
    * removed consoles and changed class to className
    
    * Enoch backend (#20)
    
    * new pull
    
    * no idea
    
    ---------
    
    Co-authored-by: ikundaE <enoch.ikunda@gmail.com>
    
    * Melissa tabs (#21)
    
    * added flag if not products in db
    
    * removed consoles and changed class to className
    
    * Melissa server route (#22)
    
    * added flag if not products in db
    
    * removed consoles and changed class to className
    
    * cart
    
    * merge
    
    * successful put request to server
    
    ---------
    
    Co-authored-by: Jonathan Amsalem <jonathan.amsalem@gmail.com>
    
    * Logo (#23)
    
    * added flag if not products in db
    
    * removed consoles and changed class to className
    
    * cart
    
    * merge
    
    * successful put request to server
    
    * new logo
    
    * updated styles to make it responsive and more cohesive
    
    * tweaks
    
    ---------
    
    Co-authored-by: Jonathan Amsalem <jonathan.amsalem@gmail.com>
    
    * Footer (#24)
    
    * added flag if not products in db
    
    * removed consoles and changed class to className
    
    * Footer2 (#25)
    
    * added flag if not products in db
    
    * removed consoles and changed class to className
    
    * cart
    
    * merge
    
    * successful put request to server
    
    * new logo
    
    * updated styles to make it responsive and more cohesive
    
    * tweaks
    
    * correct footer
    
    ---------
    
    Co-authored-by: Jonathan Amsalem <jonathan.amsalem@gmail.com>
    
    * Sticky footer (#26)
    
    * added flag if not products in db
    
    * removed consoles and changed class to className
    
    * added dependency
    
    * updated font files
    
    * removed unnecessary code
    
    * added build for fonts
    
    * moved all fonts to typography
    
    * fonts.css
    
    * fixed config folder
    
    ---------
    
    Co-authored-by: jonamsalem <133527937+jonamsalem@users.noreply.github.com>
    Co-authored-by: Jonathan Amsalem <jonathan.amsalem@gmail.com>
    Co-authored-by: ikundaE <enoch.ikunda@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        assetFileNames: "assets/[name][extname]", // This sets the output path for assets (including fonts)
+        assetFileNames: "src/fonts", // This sets the output path for assets (including fonts)
       },
     },
     assetsInclude: /\.(woff|woff2|ttf|eot|otf)$/i, // Include common font file extensions
